e8319b5bcd479b5f31f6b1274032bfb0aa17e0f5
j34ni <jeani@uio.no>
2025-05-08 19:58:59 +0000

    Rest of the recipe

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
 echo "Activating environment..."
 . /opt/conda/etc/profile.d/conda.sh && conda activate heasoft
-. $CONDA_PREFIX/headas-init.sh
+
 export PATH=$CONDA_PREFIX/bin:$PATH
 export PYTHONPATH=$CONDA_PREFIX/lib/python3.12/site-packages:$PYTHONPATH
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-exec "$@"
+export HEADAS=$CONDA_PREFIX/x86_64-pc-linux-gnu-libc2.39
+
+. $HEADAS/headas-init.sh
